0101fe7532d9d22b941df50f1e362efd28fd53e0
franchsli <112600427+franchsli@users.noreply.github.com>
2025-01-10 16:55:06 -0500

    Added rounded pills

--- static/app.js
+++ static/app.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const navbar_items = document.querySelectorAll('#navbarContent ul li')
     navbar_items.forEach((item) => {
+        const item_button = item.querySelector('button')
         item.onmouseenter = () => {
-            item.style.color = 'red'
+            item_button.classList.add('btn-primary', 'rounded-pill')
+        }
+        item.onmouseleave = () => {
+            item_button.classList.remove('btn-primary', 'rounded-pill')
         }
     })
 })
