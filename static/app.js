bd9a8a68a413177af8785d3d4bf1a30a407650ac
franchsli <112600427+franchsli@users.noreply.github.com>
2025-01-13 16:58:38 -0500

    Fixed navbar bugs

--- static/app.js
+++ static/app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const navbar_items = document.querySelectorAll('#navbarContent ul li')
     navbar_items.forEach((item) => {
-        const item_button = item.querySelector('button')
+        const item_button = item.firstElementChild
         item.onmouseenter = () => {
             item_button.classList.add('btn-info', 'rounded-pill')
         }
